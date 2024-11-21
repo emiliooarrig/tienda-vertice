61a96f6f91bfa8c99d6e222bf611e87121bd1310
emiliooarrig <earriaga@anahuac.mx>
2024-11-20 20:00:16 -0600

    Creamos archivo para destruir la sesión de usuario

---
+++ php/config/logout.php
@@ -0,0 +1,12 @@
+<?php
+session_start(); // Inicia la sesión
+
+// Destruir todas las variables de sesión
+session_unset();
+
+// Destruir la sesión
+session_destroy();
+
+// Redireccionar al usuario a la página de login o inicio
+header("Location: ../login/login.php");
+exit();
