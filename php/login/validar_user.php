4c1a25bf7b5a7cd618457dd7d0e8de239adf49de
emiliooarrig <earriaga@anahuac.mx>
2024-11-21 13:29:07 -0600

    Validamos si ingresa un usuario o admin

--- php/login/validar_user.php
+++ php/login/validar_user.php
@@ -8,29 +8,38 @@ session_start(); // Inicia la sesión
 $error = ""; // Variable para almacenar mensajes de error
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //Recuperamos los datos del input
     $username = mysqli_real_escape_string($conn, $_POST['user']);
     $password = mysqli_real_escape_string($conn, $_POST['passwd']);
 
-    // Consulta a la base de datos
-    $sql = "SELECT * FROM Administradores WHERE user_admin = '$username'";
-    $result = mysqli_query($conn, $sql);
+    // Consulta a la base de datos para consultar si es un usuario o un administrador
+    $sql = "SELECT * FROM Usuarios WHERE nombre_usuario = '$username'";
+    $sqlAdmin = "SELECT * FROM Administradores WHERE user_admin = '$username'";
 
-    if (mysqli_num_rows($result) === 1) {
-        $row = mysqli_fetch_assoc($result);
+    $resultAdmin = mysqli_query($conn, $sqlAdmin);
+    $resultUser = mysqli_query($conn, $sql);
 
-        // Verificar la contraseña
+    if (mysqli_num_rows($resultUser) === 1) {
+        $row = mysqli_fetch_assoc($resultUser);
+        if (password_verify($password, $row['contrasena'])) {
+            echo "Entra";
+            $_SESSION['user_id'] = $row['id_usuario'];
+            $_SESSION['username'] = $row['nombre_usuario'];
+            $_SESSION['role'] = 'user';
+            header('Location: ../compras/compras.php');
+            exit();
+        }
+    } elseif (mysqli_num_rows($resultAdmin) === 1) {
+        $row = mysqli_fetch_assoc($resultAdmin);
         if (password_verify($password, $row['passwd_admin'])) {
-            // Guardar datos en la sesión
             $_SESSION['admin_id'] = $row['id_admin'];
             $_SESSION['username'] = $row['user_admin'];
-
-            // Redirigir al panel de administrador
-            header('Location: ../admin/admin.php?status=succed');
+            $_SESSION['role'] = 'admin';
+            header('Location: ../admin/admin.php');
             exit();
-        } else {
-            header('Location: login.php?status=error');
         }
     } else {
         header('Location: login.php?status=error');
+        exit();
     }
 }
