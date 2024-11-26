e0b12d923a1c82fe6d25fba92555ac43adb95ad6
emiliooarrig <earriaga@anahuac.mx>
2024-11-26 01:26:13 -0600

    Modificamos form para agregar articulo

--- php/admin/agregar/agregar_art.php
+++ php/admin/agregar/agregar_art.php
@@ -5,12 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../../../style.css">
+    <link rel="stylesheet" href="../../../css/fondo.css">
     <title> Agregar articulo </title>
 </head>
 <body>
-    <div class="container mt-5">
-        <h1 class="text-center mb-4">Registrar Producto</h1>
-        <form action="../guardar.php" method="post" enctype="multipart/form-data">
+    <div class="container bg-light p-5 mt-5">
+        <h1 class="text-center mb-4 texto-rosa" style="color: #FF4D80;"> Registrar Producto </h1>
+        <form action="../guardar.php" method="post" enctype="multipart/form-data" class="bg-white p-4 shadow-lg">
             <!-- Nombre -->
             <div class="mb-3">
                 <label for="nombre" class="form-label">Nombre del Producto</label>
@@ -59,10 +61,17 @@
 
             <!-- Botón de Envío -->
             <div class="text-center">
-                <button type="submit" class="btn btn-primary">Registrar Producto</button>
+                <button type="submit" class="btn button-rosa">Registrar Producto</button>
             </div>
 
         </form>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+        crossorigin="anonymous"></script>
 </body>
 </html>
